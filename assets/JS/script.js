06e143866e71fb520066a83b79b4eb4bfe1ddcb2
allan <allankelvin.santos@hotmail.com>
2022-08-31 21:53:47 -0300

    primeiro commit

---
+++ assets/JS/script.js
@@ -0,0 +1,7 @@
+function displayAbout(){
+    arrow =document.getElementById('animation');
+    arrow.classList.toggle('rotate');
+
+    display = document.getElementById('about');
+    display.classList.toggle('active');
+}
